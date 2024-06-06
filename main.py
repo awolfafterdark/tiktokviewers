c7ff95f152f2d636bfa7f23b9d703ebe2a087cbf
awolfafterdark <32263318+awolfafterdark@users.noreply.github.com>
2024-06-06 08:49:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -28,26 +28,58 @@ except:
 from os.path import isfile
 
 text = """
-          XX                                    XX
-        XX..X                                  X..XX
-      XX.....X                                X.....XX
- XXXXX.....XX                                  XX.....XXXXX
-X |......XX%,.@                              @#%,XX......| X
-X |.....X  @#%,.@                          @#%,.@  X.....| X
-X  \...X     @#%,.@                      @#%,.@     X.../  X
- X# \.X        @#%,.@                  @#%,.@        X./  #
-  ##  X          @#%,.@              @#%,.@          X   #
-, "# #X            @#%,.@          @#%,.@            X ##
-   `###X             @#%,.@      @#%,.@             ####'
-  . ' ###              @#%.,@  @#%,.@              ###`"
-    . ";"                @#%.@#%,.@                ;"` ' .
-      '                    @#%,.@                   ,.
-      ` ,                @#%,.@  @@                `
-                          @@@  @@@                  .
-
-
-                   Lê Quang Minh (ʚMunɞ)
-                  Nhấn Enter Để Tiếp Tục 
+                                         do.
+                                        :NOX
+                                       ,NOM@:
+                                       :NNNN:
+                                       :XXXON
+                                       :XoXXX.
+                                       MM;ONO:
+  .oob..                              :MMO;MOM
+ dXOXYYNNb.                          ,NNMX:MXN
+ Mo"'  '':Nbb                        dNMMN MNN:
+ Mo  'O;; ':Mb.                     ,MXMNM MNX:
+ @O :;XXMN..'X@b.                  ,NXOMXM MNX:
+ YX;;NMMMM@M;;OM@o.                dXOOMMN:MNX:
+ 'MOONM@@@MMN:':NONb.            ,dXONM@@MbMXX:
+  MOON@M@@MMMM;;:OOONb          ,MX'"':ONMMMMX:
+  :NOOM@@MNNN@@X;""XNN@Mb     .dP"'   ,..OXM@N:
+   MOON@@MMNXXMMO  :M@@M...@o.oN"0MQOOOXNNXXOo:
+   :NOX@@@MNXXXMNo :MMMM@K"`,:;NNM@@NXM@MNO;.'N.
+    NO:X@@MNXXX@@O:'X@@@@MOOOXMM@M@NXXN@M@NOO ''b
+    `MO.'NMNXXN@@N: 'XXM@NMMXXMM@M@XO"'"XM@X;.  :b
+     YNO;'"NXXXX@M;;::"XMNN:""ON@@MO: ,;;.:Y@X: :OX.
+      Y@Mb;;XNMM@@@NO: ':O: 'OXN@@MO" ONMMX:`XO; :X@.
+      '@XMX':OX@@MN:    ;O;  :OX@MO" 'OMM@N; ':OO;N@N
+       YN;":.:OXMX"': ,:NNO;';XMMX:  ,;@@MNN.'.:O;:@X:
+       `@N;;XOOOXO;;:O;:@MOO;:O:"" ,oMP@@K"YM.;NMO;`NM
+        `@@MN@MOX@@MNMN;@@MNXXOO: ,d@NbMMP'd@@OX@NO;.'bb.
+       .odMX@@XOOM@M@@XO@MMMMMMNNbN"YNNNXoNMNMO"OXXNO.."";o.
+     .ddMNOO@@XOOM@@XOONMMM@@MNXXMMo;."' .":OXO ':.'"'"'  '""o.
+    'N@@X;,M@MXOOM@OOON@MM@MXOO:":ONMNXXOXX:OOO               ""ob.
+   ')@MP"';@@XXOOMMOOM@MNNMOO""   '"OXM@MM: :OO.        :...';o;.;Xb.
+  .@@MX" ;X@@XXOOM@OOXXOO:o:'      :OXMNO"' ;OOO;.:     ,OXMOOXXXOOXMb
+ ,dMOo:  oO@@MNOON@N:::"      .    ,;O:."'  .dMXXO:    ,;OX@XO"":ON@M@
+:Y@MX:.  oO@M@NOXN@NO. ..: ,;;O;.       :.OX@@MOO;..   .OOMNMO.;XN@M@P
+,MP"OO'  oO@M@O:ON@MO;;XO;:OXMNOO;.  ,.;.;OXXN@MNXO;.. oOX@NMMN@@@@@M:
+`' "O:;;OON@@MN::XNMOOMXOOOM@@MMNXO:;XXNNMNXXXN@MNXOOOOOXNM@NM@@@M@MP
+   :XN@MMM@M@M:  :'OON@@XXNM@M@MXOOdN@@@MM@@@@MMNNXOOOXXNNN@@M@MMMM"
+   .oNM@MM@ONO'   :;ON@@MM@MMNNXXXM@@@@M@PY@@MMNNNNNNNNNNNM@M@M@@P'
+  ;O:OXM@MNOOO.   'OXOONM@MNNMMXON@MM@@b. 'Y@@@@@@@@@@@@@M@@MP"'
+ ;O':OOXNXOOXX:   :;NMO:":NMMMXOOX@MN@@@@b.:M@@@M@@@MMM@
+ :: ;"OOOOOO@N;:  'ON@MO.'":""OOOO@@NNMN@@@. Y@@@MMM@@@@b
+ :;   ':O:oX@@O;;  ;O@@XO'   "oOOOOXMMNMNNN@MN""YMNMMM@@MMo.
+ :N:.   ''oOM@NMo.::OX@NOOo.  ;OOOXXNNNMMMNXNM@bd@MNNMMM@MM@bb    @GUINNESSGSHEP
+  @;O .  ,OOO@@@MX;;ON@NOOO.. ' ':OXN@NNN@@@@@M@@@@MNXNMM@MMM@,
+  M@O;;  :O:OX@@M@NXXOM@NOO:;;:,;;ON@NNNMM'`"@@M@@@@@MXNMMMMM@N
+  N@NOO;:oO;O:NMMM@M@OO@NOO;O;oOOXN@NNM@@'   `Y@NM@@@@MMNNMM@MM
+  ::@MOO;oO:::OXNM@@MXOM@OOOOOOXNMMNNNMNP      ""MNNM@@@MMMM@MP
+    @@@XOOO':::OOXXMNOO@@OOOOXNN@NNNNNNNN        '`YMM@@@MMM@P'
+    MM@@M:'''' O:":ONOO@MNOOOOXM@NM@NNN@P            "`SHEP'
+    ''MM@:     "' 'OOONMOYOOOOO@MM@MNNM"
+      YM@'         :OOMN: :OOOO@MMNOXM'
+      `:P           :oP''  "'OOM@NXNM'
+       `'                    GUINNESS' 
 """
 Anime.Fade(Center.Center(text), Colors.red_to_blue, Colorate.Vertical, enter=True)
 def banner():
@@ -61,10 +93,9 @@ d888888b d888888b db   dD d888888b  .d88b.  db   dD            db    db d888888b
    88      .88.   88 `88.    88    `8b  d8' 88 `88.             `8bd8'    .88.   88.     `8b d8'8b d8' 
    YP    Y888888P YP   YD    YP     `Y88P'  YP   YD               YP    Y888888P Y88888P  `8b8' `8d8'  
       
-                                Tool Buff View TikTok Version Python 2022
+                                Tool Buff View TikTok Version Python 2024
                          
-                                    Youtube: Lê Quangg Minhh Official
-
+                                    TikTok: guinnessgshep
 
 """
  for X in banner:
